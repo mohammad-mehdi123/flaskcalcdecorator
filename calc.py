824b56165041c2d85303bd9bc238592b595d14d0
smehdirizvi <mohammadmehdi.cis@gmail.com>
2018-10-04 11:57:46 +0000

    edited

--- calc.py
+++ calc.py
@@ -1,26 +1,24 @@
 
 from flask import Flask,request,jsonify
+from functools import wraps
 
 app = Flask(__name__)
 
-@app.before_request
-def inverse():
-    data = request.json
-    resp = {}
-    answer = None
-    op = data["op"]
-    op1 = data["op1"]
-    op2 = data["op2"]
-    if op == "+":
-        answer = op1-op2
-    elif op == "-":
-        answer = op1+op2
-   
-    resp["result"] = answer
-    return jsonify(resp)
+def inverse(f):
+    @wraps(f)
+    def wrapper(*arg,**kwargs):
+        data = request.json   
+        op = data["op"]
+        if op == "+":
+            request.json["op"] = "-"
+        elif op == "-":
+            request.json["op"] = "+"
+        return f(*arg,**kwargs)
+    return wrapper
 
 
 @app.route("/calc", methods=['POST'])
+@inverse
 def calculator():
     data=request.json
     op1 = data["op1"]
@@ -31,9 +29,10 @@ def calculator():
     
     if op == "+":
         answer = op1+op2
+    if op == "-":
+        answer = op1-op2
 
     resp["result"] = answer
-
     return jsonify(resp)
 
 if __name__ == '__main__':
